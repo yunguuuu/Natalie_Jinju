af3017f1c12ea5c78ab6e42a8be27211cabc5ae9
yunguuuu <gusutabogomesu@gmail.com>
2026-06-29 13:24:28 -0300

    Today's Project Update, added new 'Views' and 'components'

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,43 @@
-import { Dimensions, Image, ScrollView, StyleSheet } from 'react-native';
+import { Dimensions, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-import { Font } from '@/components/icon';
+import { Topic1, Topic2 } from '@/components/icon';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 export default function Index(){
     return(
-        <ScrollView>
+        <ScrollView style={styles.container}>
             <Image
             source={require('../assets/banner.jpg')}
             style={styles.banner}
             resizeMode='cover'
             />
+        
+        <View style={styles.div}>
 
-            <Font
-            style={styles.font}
+            <Topic1
+                style={styles.title}
             />
+           
+            <Text style={styles.paragraph}>
+                The musician american corean, Natalie Jinju, was born in September 2006; started her artistic career while she was in High School. Natalie could focus more on her career after she entered The University of Southern California's Thornton School of Music. 
+            </Text>
+
+        </View>
+        
+        <View style={styles.div}>
+            <Topic2
+                style={styles.title}
+            />
+
+            <Text style={styles.paragraph}>
+                Her first album was realesed in 2024, featuring some of her peviously realesed singles and new tracks; It is called Last September.
+            </Text>
+
+        </View>
+            
+
         </ScrollView>
     )
 }
@@ -32,8 +53,21 @@ const styles = StyleSheet.create({
         marginBottom: 20
     },
 
-    font: {
-        fontSize: 24,
-        fontWeight: 700
+    title: {
+        fontSize: 28,
+        fontWeight: 700,
+        textAlign: "center"
+    },
+
+    div: {
+        padding: 20,
+        backgroundColor: "white",
+        borderRadius: 5,
+        marginBottom: 15
+    },
+
+    paragraph: {
+        textAlign: "justify",
+        fontSize: 16,
     }
 })
--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,11 +1,22 @@
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
-export function Font({ style }: any){
+export function Topic1({ style }: any){
     return(
         <FontAwesome5 name='record-vinyl'
         style={style}
         >
-            Quem é Natalie Jinju?
+                Who is Natalie Jinju?
+        </FontAwesome5>
+
+    )
+}
+
+export function Topic2({ style }: any){
+    return(
+        <FontAwesome5 name='record-vinyl'
+        style={style}
+        >
+            First Album:
         </FontAwesome5>
 
     )
